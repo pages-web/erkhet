ba65e0205b9063745a5465ebb19160b9ad4d086e
KhBaterdene <dev@erxes.io>
2024-03-12 15:31:06 +0800

    sync

--- containers/currentOrder.tsx
+++ containers/currentOrder.tsx
@@ -1,16 +1,10 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
 import useCurrentOrder from '@/sdk/queries/order';
-import { Loader2Icon } from 'lucide-react';
 
 const CurrentOrder = () => {
-  const { loading } = useCurrentOrder();
-  return (
-    <Button className="fixed left-6 bottom-6 rounded-full" size="icon">
-      {loading && <Loader2Icon className="h-5 w-5 animate-spin" />}
-    </Button>
-  );
+  useCurrentOrder();
+  return null;
 };
 
 export default CurrentOrder;
