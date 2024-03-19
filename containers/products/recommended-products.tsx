2a5049b04d3f6055828f216a0fc5f39713257d87
KhBaterdene <dev@erxes.io>
2024-03-19 15:55:31 +0800

    delete useless exp

--- containers/products/recommended-products.tsx
+++
@@ -1,7 +0,0 @@
-import RecommendedProducts from '@/components/recommended-products/recommended-products';
-
-const RecommendedProductsContainer = () => {
-  return <RecommendedProducts />;
-};
-
-export default RecommendedProductsContainer;
