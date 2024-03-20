1f68cee0c832bc049687cf286a440f9153c5290a
KhBaterdene <dev@erxes.io>
2024-03-20 21:12:09 +0800

    for sure

--- components/ui/picture.tsx
+++ components/ui/picture.tsx
@@ -9,9 +9,7 @@ import { CommonImageProps } from './image';
 
 export function cloudflareLoader({ src, width, quality }: ImageLoaderProps) {
   const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto'];
-  return `https://erxes.io/cdn-cgi/image/${params.join(
-    ','
-  )}/${src.trim()}`.replaceAll('%20', '');
+  return `https://erxes.io/cdn-cgi/image/${params.join(',')}/${src.trim()}`;
 }
 
 const Picture = (
