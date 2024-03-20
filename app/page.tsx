75fe898e6120565bff26dfb48f68d39f8b95ed37
KhBaterdene <dev@erxes.io>
2024-03-20 18:51:06 +0800

    cleanup dummy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { CategoryCard } from '@/components/category-card/category-card';
-import Display from '@/components/display/display';
-import { Heading } from '@/components/heading/heading';
-import MainBanner from '@/components/main-banner/main-banner';
+// import { CategoryCard } from '@/components/category-card/category-card';
+// import Display from '@/components/display/display';
+// import { Heading } from '@/components/heading/heading';
+// import MainBanner from '@/components/main-banner/main-banner';
 import { getConfig } from '@/sdk/queries/auth';
 import { Metadata } from 'next/types';
 
@@ -16,15 +16,21 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default function Home() {
+export default async function Home() {
+  const { config } = await getConfig();
   return (
     <>
-      <MainBanner />
+      {/* <MainBanner />
       <Heading title="Shop By Category" />
       <CategoryCard />
       <Display />
       <Heading title="Inspired by your picks" />
-      <div className="container pb-20">{/* <RecommendedProducts /> */}</div>
+      <div className="container pb-20">
+      </div>
+     */}
+      <div className="h-screen py-36 text-center text-3xl font-bold">
+        Welcome to {config?.name}
+      </div>
     </>
   );
 }
