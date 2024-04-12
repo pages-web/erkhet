1d6c949b2e83cea2907682857750c7a75aa5ca11
Batu <dev@erxes.io>
2024-04-12 14:51:51 +0800

    remove comment

--- app/category/error.tsx
+++ app/category/error.tsx
@@ -5,14 +5,13 @@ import { useEffect } from 'react';
 
 export default function Error({
   error,
-  reset
+  reset,
 }: {
   error: Error & { digest?: string };
   reset: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
+    error && console.error(error);
   }, [error]);
 
   return (
