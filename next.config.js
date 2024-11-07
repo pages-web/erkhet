5500d98f4c26e2a432f3e783344613cd88f356ed
honilee <dev@erxes.io>
2024-11-07 13:57:13 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,26 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_MAIN_API_DOMAIN: "https://noorog.app.erxes.io/api",
+    NEXT_PUBLIC_WS_DOMAIN: "wss://noorog.app.erxes.io/api/graphql",
+    NEXT_PUBLIC_POS_TOKEN: "tUVAnqo1K8MlY5vipKvnZJsU3GBQ2ckb",
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*.cloudfront.net'
+        protocol: "https",
+        hostname: "*.cloudfront.net",
       },
       {
-        protocol: 'http',
-        hostname: 'localhost'
+        protocol: "http",
+        hostname: "localhost",
       },
       {
-        protocol: 'https',
-        hostname: '**.erxes.io'
+        protocol: "https",
+        hostname: "**.erxes.io",
       },
       {
-        protocol: 'https',
-        hostname: 'erxes.io'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "erxes.io",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
